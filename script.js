15c39ee7afb9aa5b50e9b0614a4b43c6ad1f4e8f
andre1828 <andremelo13@gmail.com>
2016-03-26 17:30:24 -0300

    Layout and visual changes
    Adds button with text box
    
    Adds margin-bottom to the input-group
    
    Changes tasks to panels

--- script.js
+++ script.js
@@ -1,81 +1,64 @@
 var panelCount = 0;
 
-   function addTask()
+   function addTask(e)
   {
-    if(document.getElementById("input").value == "") return;         // If input text box is empty, do not create task
+    if(document.getElementById("input").value == "" ) return;        // If input text box is empty or pressed key is not "enter", do not create task
 
     var newPanel = document.createElement("div");                    // Creates a new div 
     
-    newPanel.className += "well well-sm";                            // Adds class name to apply Bootstrap theme
+    newPanel.className += "panel panel-info";                        // Adds class name to apply Bootstrap theme
     
     newPanel.id =  panelCount++ ;                                    // Sets id for the current panel based on panelCount , and increments panelCount
+
+    panelHeading = document.createElement("div");                    // Creates panel's heading                   
+
+    panelHeading.className += "panel-heading";                       // Adds class to apply Bootstrap theme
+
+    var h4 = document.createElement("h4");                           // Creates h4 for heading text
+
+    h4.className += "panel-title";                                   // Adds class to apply Bootstrap theme
+
+    h4.innerText = "Task " + panelCount ;                            // Adds Task + number as title of task
+ 
+    var deleteBtn =  document.createElement("button");               // Creates button
+
+    deleteBtn.className += "btn btn-xs btn-danger";                  // Adds class to apply Bootstrap theme
+
+    SpanIcon = document.createElement("span");                       // Creates span to store "X" icon
+
+    SpanIcon.className += "glyphicon glyphicon-remove";              // Applies icon
+
+    deleteBtn.appendChild(SpanIcon);                                 // Adds icon to button
+
+    panelHeading.appendChild(h4);                                    // Adds heading to panelHeading
+
+    panelHeading.appendChild(deleteBtn);                             // Adds delete button to panelHeading
+
+    newPanel.appendChild(panelHeading);                              // Adds panelHeading to panel
+ 
+    deleteBtn.addEventListener("click", deleteTask);                 // Sets "deleteTask" to click event for this button
 
     newP = document.createElement("p");                              // Creates a p tag
     
     newP.innerText = document.getElementById("input").value;         // Gets content from input box and applies to newP
 
     newPanel.appendChild(newP);                                      // Adds the p tag  to the panel
+                                      
+    document.getElementById("taskList").appendChild(newPanel);       // Adds newPanel to 
     
-    var newBtn = document.createElement("button");                   // Creates a new button
-                                        
-    newBtn.className += "btn btn-xs btn-danger";                     // Sets button's class to apply Boostrap theme                 
-    
-    newSpanIcon = document.createElement("span");                    // Creates a span to recieve the "X" icon
-    
-    newSpanIcon.className += "glyphicon glyphicon-remove";           // Adds the "X" icon to the span
-    
-    newBtn.appendChild(newSpanIcon);                                 // Adds the icon to the button
-    
-    newBtn.addEventListener("click", deleteTask);                    // Sets "deleteTask" to click event for this button
-    
-    newPanel.appendChild(newBtn);                                    // 
-    
-    document.getElementById("taskList").appendChild(newPanel); // Adds new panel to the row 
     }
 
 
   function deleteTask()
   {
-    var id = this.parentNode.id;
-    document.getElementById(id).remove();
+    var id = this.parentNode.parentNode.id;                          // Gets id of panel
+    document.getElementById(id).remove();                            // Deletes the panel
+    panelCount--;                                                    // decreases panelCount
   }
 
-  function setTaskAsDone()
+  function setTaskAsDone()   /* TODO */
   {
     var id = this.parentNode.id;
     var currentText = document.getElementById(id).innerText;
     document.getElementById(id).innerText = currentText.del();
-  }
-
-  function testeInput(e)
-  {
-    if(document.getElementById("input").value == "" || e.keyCode != 13) return;         // If input text box is empty or pressed key is not "enter", do not create task
-
-    var newPanel = document.createElement("div");                    // Creates a new div 
-    
-    newPanel.className += "well well-sm";                            // Adds class name to apply Bootstrap theme
-    
-    newPanel.id =  panelCount++ ;                                    // Sets id for the current panel based on panelCount , and increments panelCount
-
-    newP = document.createElement("p");                              // Creates a p tag
-    
-    newP.innerText = document.getElementById("input").value;         // Gets content from input box and applies to newP
-
-    newPanel.appendChild(newP);                                      // Adds the p tag  to the panel
-    
-    var newBtn = document.createElement("button");                   // Creates a new button
-                                        
-    newBtn.className += "btn btn-xs btn-danger";                     // Sets button's class to apply Boostrap theme                 
-    
-    newSpanIcon = document.createElement("span");                    // Creates a span to recieve the "X" icon
-    
-    newSpanIcon.className += "glyphicon glyphicon-remove";           // Adds the "X" icon to the span
-    
-    newBtn.appendChild(newSpanIcon);                                 // Adds the icon to the button
-    
-    newBtn.addEventListener("click", deleteTask);                    // Sets "deleteTask" to click event for this button
-    
-    newPanel.appendChild(newBtn);                                    // 
-    
-    document.getElementById("taskList").appendChild(newPanel); // Adds new panel to the row 
   }
